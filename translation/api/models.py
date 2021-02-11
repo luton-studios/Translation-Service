0cabd3a42d315cdabc5d1f13c2f228e33bf10462
Cynthia <s38ren@uwaterloo.ca>
2021-02-11 15:15:31 -0500

    add db uniqueness constraints

--- translation/api/models.py
+++ translation/api/models.py
@@ -4,4 +4,9 @@ from django.db import models
 class Translation(models.Model):
   key = models.TextField()
   language = models.TextField()
-  phrase = models.TextField()
+  phrase = models.TextField(default="")
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=['key', 'language'], name='unique translation')
+    ]
